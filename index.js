6c89d398a958bdcc9fd6125529a23a24a4a6fd21
Alexander Petrov <acepetrov96@gmail.com>
2020-09-30 21:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+<<<<<<< HEAD
 function deleteFromObjectByKey(object, key) {
   var obj = Object.assign({}, object);
   delete obj[key];
@@ -17,3 +18,5 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
+=======
+>>>>>>> 057a377f4f7e63f08ccf4473443b6ba276c59a86
